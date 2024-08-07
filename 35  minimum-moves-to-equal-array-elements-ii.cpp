8f45606f63069565c717b08b2d31e626757e1903
Shivani Bhandari <shivanibhandari767@gmail.com>
2024-08-07 23:16:37 +0530

    35  minimum-moves-to-equal-array-elements-ii
    Given an integer array nums of size n, return the minimum number of moves required to make all array elements equal.
    
    In one move, you can increment or decrement an element of the array by 1.
    
    Test cases are designed so that the answer will fit in a 32-bit integer.
    
     
    
    Example 1:
    
    Input: nums = [1,2,3]
    Output: 2
    Explanation:
    Only two moves are needed (remember each move increments or decrements one element):
    [1,2,3]  =>  [2,2,3]  =>  [2,2,2]
    Example 2:
    
    Input: nums = [1,10,2,9]
    Output: 16
     
    
    Constraints:
    
    n == nums.length
    1 <= nums.length <= 105
    -109 <= nums[i] <= 109

---
+++ 35  minimum-moves-to-equal-array-elements-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int median = nums[n / 2];
+        int moves = 0;
+        for (int num : nums) {
+            moves += abs(num - median);
+        }
+        return moves;
+    }
+};
