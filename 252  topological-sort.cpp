437cf74f3b037966800ef4d6e22c6e8dc43be06d
Shivani Bhandari <shivanibhandari767@gmail.com>
2024-09-12 03:26:51 +0530

    252  topological-sort

---
+++ 252  topological-sort.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+	public:
+       
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    vector<int>ans;
+
+	 vector<int>ind(V , 0);
+	 for(int i=0; i<V; i++){
+	     
+	     for(auto it: adj[i]){
+	       
+	         ind[it]++;
+	     }
+	 }
+	queue<int> q;
+	 for(int i=0; i<V; i++){
+	    if(ind[i]==0)q.push(i);
+	 }
+	 
+	  while(!q.empty()){
+	      int node = q.front();
+	      q.pop();
+	      ans.push_back(node);
+	       for(auto it : adj[node]){
+	         ind[it]--;
+	         if(ind[it]==0)q.push(it);
+	       }
+	    
+	      }
+	  
+	 
+	 return ans;
+	 
+	 
+	}
+};
